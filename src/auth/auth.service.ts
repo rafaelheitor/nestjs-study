84ac4eeb8f684c303fefdaaec005f6e5497959b3
Rafael Heitor <rafaelheitor2@outlook.com>
2022-06-06 16:21:36 -0300

    Registrando usuario no banco

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { AuthDto } from "./dto";
+import * as argon from 'argon2'
 
 @Injectable()
 export class AuthService{
 
     constructor(private prisma: PrismaService){}
 
-    signup() {
+   async signup(dto: AuthDto) {
+        const hash = await argon.hash(dto.password);
+
+        const user = this.prisma.user.create({
+            data: {
+                email: dto.email,
+                hash
+            }
+        })
+        
+        return user
     
-        return {message: 'I have signed up'}
     }
     
     signin() {
