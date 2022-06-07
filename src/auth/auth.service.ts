4a16c1c3e39d74617630e2b76d5fec514ab2eb4a
unknown <rafaelheitor2@outlook.com>
2022-06-06 21:17:23 -0300

    Transformando o email em único e lidando com erro

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService{
@@ -9,6 +10,8 @@ export class AuthService{
     constructor(private prisma: PrismaService){}
 
    async signup(dto: AuthDto) {
+
+    try{
         const hash = await argon.hash(dto.password);
 
         const user = this.prisma.user.create({
@@ -17,10 +20,19 @@ export class AuthService{
                 hash
             }
         })
-        
+        delete (await user).hash
         return user
-    
+        
+    } catch(error){
+        if(error instanceof PrismaClientKnownRequestError){
+            if(error.code === 'P2002') {
+                throw new ForbiddenException('Email em uso')
+            }
+        }
+        throw error
     }
+}
+
     
     signin() {
         return {message: 'I have signed in'}
