73cd9e72e47b89e5818e9eff4b9aac05ff268a31
Rafael Heitor <rafaelheitor2@outlook.com>
2023-01-24 15:20:32 -0300

    Adding methods to User entity

--- src/core/domain/user/entity/user.entity.spec.ts
+++ src/core/domain/user/entity/user.entity.spec.ts
@@ -1,11 +1,12 @@
 import { User } from './user.entity';
 
 describe('Tests user entity', () => {
-  it('Should create a new user object', () => {
-    const user = User.new({
+  it('Should create a new user object', async () => {
+    const user = await User.new({
       name: 'Rafael',
       email: 'rafael@email.com',
       password: '123456',
     });
+    expect(user.password).toEqual('123456');
   });
 });
--- src/core/domain/user/entity/user.entity.ts
+++ src/core/domain/user/entity/user.entity.ts
@@ -1,5 +1,7 @@
-import { IsString } from 'class-validator';
+import { Entity } from '../../../common/entity/Entity';
+import { IsEmail, IsString } from 'class-validator';
 import { v4 } from 'uuid';
+import { compare, genSalt, hash } from 'bcrypt';
 
 export type createUserPayload = {
   id?: string;
@@ -8,7 +10,7 @@ export type createUserPayload = {
   password: string;
 };
 
-export class User {
+export class User extends Entity<string> {
   @IsString()
   public id: string;
 
@@ -16,24 +18,40 @@ export class User {
   public name: string;
 
   @IsString()
+  @IsEmail()
   public email: string;
 
   @IsString()
   public password: string;
 
   private constructor(payload: createUserPayload) {
+    super();
     this.id = payload.id || v4();
     this.name = payload.name;
     this.email = payload.email;
     this.password = payload.password;
   }
 
-  public static new(payload: createUserPayload): User {
-    const user: User = new User(payload);
-    return user;
+  async hashPassword(): Promise<void> {
+    const salt: string = await genSalt();
+    this.password = await hash(this.password, salt);
+    await this.validate();
   }
 
-  editPassword(password: string) {
-    return (this.password = password);
+  async editPassword(password: string): Promise<void> {
+    const salt: string = await genSalt();
+    this.password = await hash(password, salt);
+    await this.validate();
+  }
+
+  async comparePassword(password: string): Promise<boolean> {
+    return compare(password, this.password);
+  }
+
+  public static async new(payload: createUserPayload): Promise<User> {
+    const user: User = new User(payload);
+    await user.hashPassword();
+    await user.validate();
+    return user;
   }
 }
