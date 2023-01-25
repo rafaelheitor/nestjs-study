706bf731cc4bc0410209b1b984b537aeed0c62c2
Rafael Heitor <rafaelheitor2@outlook.com>
2023-01-24 22:08:44 -0300

    fix: getting the user email through a method

--- src/infrastructure/adapter/persistence/UserRepositoryAdapter.ts
+++ src/infrastructure/adapter/persistence/UserRepositoryAdapter.ts
@@ -25,7 +25,7 @@ export class UserRepositoryInMemory implements UserRepositoryPort {
     throw new Error('Method not implemented.');
   }
   async getByEmail(email: string): Promise<User> | undefined {
-    const user: User = this.users.find((user) => user.email === email);
+    const user: User = this.users.find((user) => user.getEmail() === email);
     return user;
   }
   delete(id: string): Promise<User> {
