0f1a80cb2f97f6aa50f53043aa4208ba9a665191
Rafael Heitor <rafaelheitor2@outlook.com>
2023-01-26 15:50:17 -0300

    fix: refactoring UserUseCaseDto test

--- src/core/domain/user/usecase/dto/UserUscaseDto.spec.ts
+++ src/core/domain/user/usecase/dto/UserUscaseDto.spec.ts
@@ -2,18 +2,18 @@ import { createUserPayload, User } from '../../entity/user.entity';
 import { UserUsecaseDto } from './UserUsecaseDto';
 
 describe('Tests the UserUsecaseDto', () => {
-  it('should create a new user dto', () => {
-    const user = createUser();
+  it('should create a new user dto', async () => {
+    const user = await createUser();
     const userDto: UserUsecaseDto = UserUsecaseDto.newFromUser(user);
 
     expect(user !== userDto).toBeTruthy();
   });
 
-  it('Should Create a list from User dto', () => {
-    const user: User = createUser();
-    const user2: User = createUser();
-    const user3: User = createUser();
-    const user4: User = createUser();
+  it('Should Create a list from User dto', async () => {
+    const user: User = await createUser();
+    const user2: User = await createUser();
+    const user3: User = await createUser();
+    const user4: User = await createUser();
     const users = [{ ...user }, { ...user2 }, { ...user3 }, { ...user4 }];
 
     const listFromDto: UserUsecaseDto[] = UserUsecaseDto.newListFromUser(users);
@@ -21,12 +21,12 @@ describe('Tests the UserUsecaseDto', () => {
   });
 });
 
-function createUser(id?: string) {
+async function createUser(id?: string) {
   const userPayload: createUserPayload = {
     name: 'Rafael',
     email: 'rafael@email.com',
     password: '123456',
   };
 
-  return User.new(userPayload);
+  return await User.new(userPayload);
 }
