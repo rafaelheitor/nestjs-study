a1c1488521538886855930b0657ba3d565dd2904
Rafael Heitor <rafaelheitor2@outlook.com>
2023-01-27 16:08:58 -0300

    feat: adding CoreAssert test for isFalse

--- src/core/common/util/assert/CoreAssert.spec.ts
+++ src/core/common/util/assert/CoreAssert.spec.ts
@@ -17,4 +17,20 @@ describe('CoreAssert', () => {
       }
     });
   });
+
+  describe('isFalse', () => {
+    test("When expressions is false, expect it doesn' throw a exception", () => {
+      expect(CoreAssert.isFalse(false, AssertionError)).toBeUndefined();
+    });
+
+    test('When expression is not false, expect it throw a exception', () => {
+      expect.hasAssertions();
+
+      try {
+        CoreAssert.isFalse(true, AssertionError);
+      } catch (error) {
+        expect(error).toBe(AssertionError);
+      }
+    });
+  });
 });
