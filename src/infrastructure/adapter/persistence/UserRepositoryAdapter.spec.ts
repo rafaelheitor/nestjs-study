fe7426d04e5ef3bf5c79f0c7a592b8c8e0c21e1d
Rafael Heitor <rafaelheitor2@outlook.com>
2023-02-03 14:08:06 -0300

    adding tests for UserRepositoryInMemory

---
+++ src/infrastructure/adapter/persistence/UserRepositoryAdapter.spec.ts
@@ -0,0 +1,72 @@
+import { User } from '@core/domain/user/entity/User';
+import { UserRepositoryInMemory } from './UserRepositoryAdapter';
+
+describe('UserRepositoryInMemory', () => {
+  const expectedUser = {
+    email: 'email@email.com',
+    name: 'user',
+    password: '123456',
+  };
+  const userRepository = new UserRepositoryInMemory();
+
+  test('Should save user', async () => {
+    const user: User = await createUser();
+
+    await userRepository.save(user);
+
+    expect(userRepository.users.length > 0);
+  });
+
+  test('Should edit user', async () => {
+    const user: User = await createUser();
+    jest.spyOn(userRepository, 'getByEmail').mockImplementation(async () => {
+      return user;
+    });
+
+    await user.edit({
+      name: 'João',
+      password: 'qwerty',
+    });
+    await userRepository.edit(user);
+    const savedUser = await userRepository.getByEmail(expectedUser.email);
+    expect(savedUser.getName()).toBe('João');
+    expect(savedUser.comparePassword('qwerty')).toBeTruthy;
+  });
+
+  test('Should return user if exists in the repository', async () => {
+    const user: User = await createUser();
+
+    jest.spyOn(userRepository, 'getByEmail').mockImplementation(async () => {
+      return user;
+    });
+
+    const foundUser: User = await userRepository.getByEmail(user.getEmail());
+
+    expect(foundUser).toEqual(user);
+  });
+
+  test('Should delete user', async () => {
+    const user: User = await createUser();
+    jest.spyOn(userRepository, 'getByEmail').mockImplementation(async () => {
+      return user;
+    });
+
+    await userRepository.delete(user.getEmail());
+
+    const isUserDelete = userRepository.users.findIndex(
+      (MemoryUser) => MemoryUser.getEmail() === user.getEmail(),
+    );
+
+    expect(isUserDelete).toBe(-1);
+  });
+});
+
+async function createUser() {
+  const user: User = await User.new({
+    email: 'email@email.com',
+    name: 'user',
+    password: '123456',
+  });
+
+  return user;
+}
