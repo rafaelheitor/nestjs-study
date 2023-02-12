0172991856f25e0b5c07d9aec223d03c082e6263
Rafael Heitor <rafaelheitor2@outlook.com>
2023-02-12 12:03:34 -0300

    docs: fixing Edit product body model

--- src/application/api/http-rest/controller/documentation/product/HttpRestApiEditProductBody.ts
+++ src/application/api/http-rest/controller/documentation/product/HttpRestApiEditProductBody.ts
@@ -1,15 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class HttpRestApiEditProductBody {
-  @ApiProperty({ type: 'string' })
+  @ApiProperty({ type: 'string', required: false })
   public name: string;
 
-  @ApiProperty({ type: 'string' })
+  @ApiProperty({ type: 'string', required: false })
   public image: string;
 
-  @ApiProperty({ type: 'number' })
+  @ApiProperty({ type: 'number', required: false })
   public price: number;
 
-  @ApiProperty({ type: 'number' })
+  @ApiProperty({ type: 'number', required: false })
   public quantity: number;
 }
